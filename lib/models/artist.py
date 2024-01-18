cacaf03926e359a67c2c9250db6cc0852480999d
Hana Sambora <grace.sambora@gmail.com>
2024-01-18 13:09:37 -0600

    Add nationality and movement artist attr

--- lib/models/artist.py
+++ lib/models/artist.py
@@ -1,9 +1,11 @@
 from models.__init__ import CURSOR, CONN
 
 class Artist:
-    def __init__(self, name, id = None):
+    def __init__(self, name, nationality, movement, id = None):
         self.id = id
         self.name = name
+        self.nationality = nationality
+        self.movement = movement
 
     @property
     def name(self):
@@ -16,4 +18,30 @@ class Artist:
         else:
             raise ValueError(
                 "Name must be a non-empty string"
+            )
+    
+    @property
+    def nationality(self):
+        return self._nationality
+    
+    @nationality.setter
+    def nationality(self, nationality):
+        if isinstance(nationality, str) and len(nationality):
+            self._nationality = nationality
+        else:
+            raise ValueError(
+                "Nationality must be a non-empty string"
+            )
+    
+    @property
+    def movement(self):
+        return self._movement
+    
+    @movement.setter
+    def movement(self, movement):
+        if isinstance(movement, str) and len(movement):
+            self._movement = movement
+        else:
+            raise ValueError(
+                "Movement must be a non-empty string"
             )
