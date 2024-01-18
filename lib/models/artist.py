0532639724de4ecbb0194164338da43cef76cf08
Hana Sambora <grace.sambora@gmail.com>
2024-01-18 13:03:22 -0600

    Made init method and name attr

--- lib/models/artist.py
+++ lib/models/artist.py
@@ -1,1 +1,19 @@
 from models.__init__ import CURSOR, CONN
+
+class Artist:
+    def __init__(self, name, id = None):
+        self.id = id
+        self.name = name
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, name):
+        if isinstance(name, str) and len(name):
+            self._name = name
+        else:
+            raise ValueError(
+                "Name must be a non-empty string"
+            )
