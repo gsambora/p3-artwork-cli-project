0d047c4e4006bd168928ec6073e40359ee6608c7
Hana Sambora <grace.sambora@gmail.com>
2024-01-18 13:56:10 -0600

    Add comments to all current methods

--- lib/models/artist.py
+++ lib/models/artist.py
@@ -1,14 +1,17 @@
 from models.__init__ import CURSOR, CONN
 
 class Artist:
+    #Dictionary of data from all artist class instances
     all = {}
 
     def __init__(self, name, nationality, movement, id = None):
+        #Each artist instance should have a name, nationality, and movement
         self.id = id
         self.name = name
         self.nationality = nationality
         self.movement = movement
 
+    #Setting up name, nationality, and movement as attributes
     @property
     def name(self):
         return self._name
@@ -50,6 +53,7 @@ class Artist:
     
     @classmethod
     def create_table(cls):
+        #Create an artists table in the database to hold data from the artist class instances
         sql = """
             CREATE TABLE IF NOT EXISTS artists(
             id INTEGER PRIMARY KEY,
@@ -62,6 +66,7 @@ class Artist:
 
     @classmethod
     def drop_table(cls):
+        #Drop the artists table from the database
         sql = """
             DROP TABLE IF EXISTS artists
         """
@@ -69,6 +74,7 @@ class Artist:
         CONN.commit()
     
     def save(self):
+        #Save the current instance of the artist class as a row in the artists table and set the id attribute based on position in the table
         sql = """
             INSERT INTO artists (name, nationality, movement)
             VALUES (?, ?, ?)
@@ -81,7 +87,33 @@ class Artist:
 
     @classmethod
     def create(cls, name, nationality, movement):
+        #Create a new instance of the artist class and call the save method
         artist = cls(name, nationality, movement)
         artist.save()
 
         return artist
+    
+    def update(self):
+        #Update current artist's name, nationality, and movement
+        sql = """
+            UPDATE artists
+            SET name = ?, nationality = ?, movement = ?
+            WHERE id = ? 
+        """
+        CURSOR.execute(sql, (self.name, self.nationality, self.movement, self.id))
+        CONN.commit()
+    
+    def delete(self):
+        #Delete the current artist from the artists table and class dictionary
+        sql = """
+            DELETE FROM artists
+            WHERE id = ?
+        """
+        CURSOR.execute(sql, (self.id))
+        CONN.commit()
+
+        #Delete dictionary entry using the id
+        del type(self).all[self.id]
+
+        #Set the id to none
+        self.id = None
