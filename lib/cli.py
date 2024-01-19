ff62debd284b102234aea8838c9549fe3468cd3b
Hana Sambora <grace.sambora@gmail.com>
2024-01-19 12:01:07 -0600

    Add helper functions for finding artists

--- lib/cli.py
+++ lib/cli.py
@@ -16,7 +16,11 @@ def main():
         elif choice == "1":
             list_artists()
         elif choice == "2":
-            list_all_works()
+            pass
+        elif choice == "3":
+            list_movement()
+        elif choice == "4":
+            pass #list_nationality()
         else:
             print("Invalid choice")
 
@@ -24,35 +28,35 @@ def main():
 def menu():
     print("Please select an option:")
     print("0.  Exit the program")
-
     print("1.  List all artists")
-    print("2.  Find an artist")
-    # print("1. Find artist by name")
-    # print("2. Find artist by ID")
-    # print("3. Find artist by work")
-
+    print("2.  Find an artist") #Find artist menu
     print("3.  List artists by movement")
     print("4.  List artists by nationality")
-
     print("5.  List all works of art")
-    print("6.  Find a work of art")
-    # print("1. Find work by title")
-    # print("2. Find work by ID")
-
-    print("10. List works by artist")
-    print("11. List works by medium")
-    print("12. List works in chronological order")
-
-    print("13. Add, update, or remove database information")
-    # print("1. Add an artist to the database")
-    # print("2. Update an artist's information")
-    # print("3. Remove an artist from the database")
-
-    # print("4. Add a work of art to the database")
-    # print("5. Update a work's information")
-    # print("6. Remove a work of art from the database")
+    print("6.  Find a work of art") #Find work menu
+    print("7. List works by artist")
+    print("8. List works by medium")
+    print("9. List works in chronological order")
+    print("10. Add, update, or remove database information") #Database menu
 
 
+def find_artist_menu():
+    print("1. Find artist by name")
+    print("2. Find artist by ID")
+    print("3. Find artist by work")
+
+def find_work_menu():
+    print("1. Find work by title")
+    print("2. Find work by ID")
+
+def database_menu():
+    print("1. Add an artist to the database")
+    print("2. Update an artist's information")
+    print("3. Remove an artist from the database")
+
+    print("4. Add a work of art to the database")
+    print("5. Update a work's information")
+    print("6. Remove a work of art from the database")
 
 if __name__ == "__main__":
     main()
--- lib/helpers.py
+++ lib/helpers.py
@@ -7,10 +7,45 @@ def list_artists():
     for artist in artists:
         print(artist)
 
+def artist_by_name():
+    name = input("Enter an artist's name: ")
+    artist = Artist.find_by_name(name)
+
+    if artist :
+        print(artist)
+    else:
+        print(f"Artist {name} not in database.")
+
+def artist_by_id():
+    id = input("Enter an artist's ID: ")
+    artist = Artist.find_by_id(id)
+
+    if artist:
+        print(artist)
+    else:
+        print(f"Artist {id} not in database.")
+
+def artist_by_work():
+    title = input("Enter the title of a work of art: ")
+    work = Work.find_by_title(title)
+
+    if work:
+        artist = Artist.find_by_id(work.artist_id)
+        print(f"Work {title} was created by: {artist}")
+
 def list_all_works():
     works = Work.get_all()
     for work in works:
         print(work)
+
+def list_movement():
+    input = input("Enter an artistic movement: ")
+    
+    artists = Artist.get_all()
+    for artist in artists:
+        if artist.movement == input :
+            print(artist)
+
 
 def exit_program():
     print("Goodbye!")
